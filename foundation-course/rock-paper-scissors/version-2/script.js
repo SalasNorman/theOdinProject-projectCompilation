18ac6e967c29c0a10811f051c58856aea888ee66
SalasNorman <salasnorman015@gmail.com>
2025-12-31 14:10:20 +0800

    refactor code to ui

--- foundation-course/rock-paper-scissors/version-2/script.js
+++ foundation-course/rock-paper-scissors/version-2/script.js
@@ -1,12 +1,16 @@
 let humanScore = 0;
 let computerScore = 0;
-// let humanChoice = "";
-// let computerChoice = "";
+let humanChoice = "";
+let computerChoice = "";
+let result = "";
+let run5Times = 1;
+
+let history = document.querySelector(".history");
+let totalScore = document.querySelector(".score");
 
 // Computer Input
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3 + 1);
-  let computerChoice = "";
   if (randomNum === 1) {
     computerChoice = "ROCK";
   } else if (randomNum === 2) {
@@ -14,64 +18,77 @@ function getComputerChoice() {
   } else {
     computerChoice = "SCISSORS";
   }
-  return computerChoice;
 }
 
-// Human Input
-function getHumanChoice() {
-  let humanChoice = prompt("Choose one: \nROCK, PAPER, SCISSORS");
-  return humanChoice;
+// Result
+function addHistory(result) {
+  let addHist = document.createElement("li");
+  addHist.textContent = `${result}: ${humanChoice} vs ${computerChoice}`;
+  history.appendChild(addHist);
 }
 
 // Human vs Computer
-function playRound(humanChoice, computerChoice) {
+function playRound() {
   humanChoice = humanChoice.toUpperCase();
   if (
     (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
     (humanChoice === "PAPER" && computerChoice === "ROCK") ||
     (humanChoice === "SCISSORS" && computerChoice === "PAPER")
   ) {
-    alert(humanChoice + " beat " + computerChoice + "\nYou WIN!");
-    humanScore += 1;
+    totalScore.textContent = humanScore += 1;
+    addHistory("WIN");
   } else if (
     (humanChoice === "ROCK" && computerChoice === "ROCK") ||
     (humanChoice === "PAPER" && computerChoice === "PAPER") ||
     (humanChoice === "SCISSORS" && computerChoice === "SCISSORS")
   ) {
-    alert(humanChoice + " vs " + computerChoice + "\na TIE!");
+    addHistory("DRAW");
   } else {
-    alert(computerChoice + " beat " + humanChoice + "\nYou lose! ");
     computerScore += 1;
+    addHistory("LOSS");
   }
 }
 
+let gameRun = 0;
+// run 5 times then show result
+function result5run() {
+  let resulText = "";
+  if (run5Times % 5 === 0) {
+    gameRun++;
+    if (humanScore > computerScore) {
+      resulText = `Game ${gameRun}: You WIN!`;
+    } else if (humanScore < computerScore) {
+      resulText = `Game ${gameRun}: You MOTHERF**KING LOSER!`;
+    } else {
+      resulText = `Game ${gameRun}: Its a DRAW`;
+    }
+    const h1 = document.querySelector("h1");
+    const displayresult = document.createElement("div");
+    displayresult.textContent = `${resulText}`;
+    displayresult.classList.add("displayresult");
+    h1.after(displayresult);
+
+  } else if ((run5Times - 1) % 5 === 0 && run5Times > 5) {
+    const displayresultText = document.querySelector(".displayresult");
+    displayresultText.remove();
+  }
+}
 // play the game
 function playGame() {
-  alert("Welcome to ROCK-PAPER-SCISSORS \nPlay 5 Round");
-
-  for (let i = 1; i <= 5; i++) {
-    let humanPlay = getHumanChoice;
-    let computerPlay = getComputerChoice;
-    playRound(humanPlay(), computerPlay());
-  }
-
-  if (humanScore > computerScore) {
-    alert(
-      "You: " + humanScore + " Com: " + computerScore + "\nYour the WINNER!"
-    );
-  } else if (humanScore < computerScore) {
-    alert(
-      "You: " +
-        humanScore +
-        " Com: " +
-        computerScore +
-        "\nYou MOTHERF**KING LOSER!"
-    );
-  } else {
-    alert(
-      "You: " + humanScore + " Com: " + computerScore + "\na TIE! \nTRY AGAIN"
-    );
-  }
+  let computerPlay = getComputerChoice;
+  playRound(computerPlay());
+  result5run();
 }
 
-playGame();
+// human Input
+const div = document.querySelector("div");
+
+div.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    // console.history(event.target.outerHTML);
+    // console.history(event.target.textContent);
+    humanChoice = event.target.textContent;
+    playGame();
+    run5Times++;
+  }
+});
