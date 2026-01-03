0b3369ce7bac8a38b5421be411dca8d2941e6703
SalasNorman <salasnorman015@gmail.com>
2026-01-03 18:08:28 +0800

    add reset grid when range value change

--- foundation-course/etch-a-sketch/script.js
+++ foundation-course/etch-a-sketch/script.js
@@ -2,16 +2,14 @@ const container = document.querySelector(".container");
 const gridSizeLabel = document.querySelector("#grid-size-display");
 const gridSizeInput = document.querySelector("#grid-size");
 
-let defaultGridSize = 30;
+let defaultGridSize = 16;
 
 gridSizeLabel.textContent = defaultGridSize;
 
 container.addEventListener("mouseover", (event) => {
   let target = event.target;
-  console.log(target);
   if (target.className !== "container") {
     target.classList.add("highlight");
-    console.log(target);
   }
 });
 
@@ -20,6 +18,10 @@ gridSizeLabel.textContent = gridSizeInput.value;
 gridSizeInput.addEventListener("input", (event) => {
   defaultGridSize = event.target.value;
   gridSizeLabel.textContent = defaultGridSize;
+  console.log(defaultGridSize);
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
   generateGrid();
 });
 // }
@@ -28,9 +30,8 @@ function generateGrid() {
   let gridSize = defaultGridSize * defaultGridSize;
   for (let i = 1; i <= gridSize; i++) {
     const grid = document.createElement("div");
-    let hw = 352 / defaultGridSize -2;
+    let hw = 352 / defaultGridSize - 2;
     grid.classList.add("box");
-    console.log(hw)
     grid.style.height = `${hw}px`;
     grid.style.width = `${hw}px`;
     container.appendChild(grid);
